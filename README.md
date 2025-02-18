6b6a1084f9d4bbf0ca62203bab8c70ea8016800e
Andrew Wong <awong6@atlassian.com>
2025-02-19 00:31:24 +1100

    pressed buttons on keyboard

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # macOS EPM JIT Log
 
+Logs events when you perform the "Request Administrative Privileges" action in CyberArk EPM.  
+Assumes requests are always granted (i.e. through the JIT approver service)
+
+---
+
+## Log Types
+
+* `<epoch:ms>,request` - Request made
+* `<epoch:ms>,request,<interval:ms>` - A new request was made `interval` ms after the previous request, before a grant was received
+* `<epoch:ms>,requestPending,<interval:ms>` - A request created `interval` ms ago is still pending
+  * This only appears after `120` seconds
+* `<epoch:ms>,grant:<interval:ms>` - A request was granted after `interval` ms
+* `<epoch:ms>,revoke` - Administrative privileges have been revoked
--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,41 @@
 const std = @import("std");
+const ctime = @cImport({
+    @cInclude("time.h");
+});
+
 const ChildProcess = std.process.Child;
 
 const LogStream = struct { eventMessage: []const u8, subsystem: []const u8, processID: c_int, timestamp: []const u8 };
 
-const AppEvent = struct { timeString: []const u8, type: []const u8, hasDelta: bool, delta: i64 };
+const AppEvent = struct { timeMs: i64, type: []const u8, hasDelta: bool, delta: i64 };
 
 const stdout = std.io.getStdOut().writer();
 const stderr = std.io.getStdErr().writer();
 
 pub fn emitEvent(event: AppEvent) !void {
     if (event.hasDelta) {
-        try stdout.print("{s},{s},{d}\n", .{ event.timeString, event.type, event.delta });
+        try stdout.print("{d},{s},{d}\n", .{ event.timeMs, event.type, event.delta });
     } else {
-        try stdout.print("{s},{s}\n", .{ event.timeString, event.type });
+        try stdout.print("{d},{s}\n", .{ event.timeMs, event.type });
+    }
+}
+var reqStartTimeMs: i64 = 0;
+
+fn deltaMonitor() !void {
+    const waitSeconds = 10;
+    const thresholdSeconds = 120;
+
+    while (true) {
+        std.time.sleep(waitSeconds * std.time.ns_per_s);
+
+        if (reqStartTimeMs > 0) {
+            const now = std.time.milliTimestamp();
+            const delta = now - reqStartTimeMs;
+
+            if (delta > thresholdSeconds * std.time.ms_per_s) {
+                try emitEvent(AppEvent{ .timeMs = now, .type = "requestPending", .hasDelta = true, .delta = delta });
+            }
+        }
     }
 }
 
@@ -20,7 +43,7 @@ pub fn main() !void {
     var allocatorBacking = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = allocatorBacking.allocator();
 
-    const filter = "sender == 'com.cyberark.CyberArkEPMEndpointSecurityExtension' AND ((eventMessage == 'Requested administrative privileges') OR (eventMessage BEGINSWITH 'Did ' AND eventMessage ENDSWITH \"to '_cyberarkepm_sudoers' group\"))";
+    const filter = "subsystem == 'com.cyberark.CyberArkEPM' AND ((formatString == 'Requested administrative privileges') OR (eventMessage ENDSWITH \" 'admin' group\" AND (formatString BEGINSWITH 'ac_%llu: Did add ' OR formatString BEGINSWITH 'ac_%llu: Did remove ')))";
 
     var proc = ChildProcess.init(&[_][]const u8{ "/usr/bin/log", "stream", "--style", "ndjson", "--info", "--predicate", filter }, allocator);
     proc.stdout_behavior = ChildProcess.StdIo.Pipe;
@@ -37,33 +60,39 @@ pub fn main() !void {
 
     try stderr.print("Observing events...\n", .{});
 
-    var reqStartTimeMs: i64 = 0;
+    _ = try std.Thread.spawn(.{}, deltaMonitor, .{});
 
     while (true) {
         const bytesRead = (try reader.readUntilDelimiter(&buffer, '\n')).len;
         const parsed = try std.json.parseFromSlice(LogStream, allocator, buffer[0..bytesRead], .{ .ignore_unknown_fields = true });
         defer parsed.deinit();
 
-        var evtObject = AppEvent{ .timeString = parsed.value.timestamp, .type = undefined, .hasDelta = false, .delta = 0 };
+        const now = std.time.milliTimestamp();
+
+        var evtObject = AppEvent{ .timeMs = now, .type = undefined, .hasDelta = false, .delta = 0 };
 
         if (std.mem.startsWith(u8, parsed.value.eventMessage, "Requested")) {
             evtObject.type = "request";
 
-            // There is another outstanding request, calculate the delta between requests
+            // There is another outstanding request, show the interval between requests
             if (reqStartTimeMs > 0) {
                 evtObject.hasDelta = true;
-                evtObject.delta = std.time.milliTimestamp() - reqStartTimeMs;
+                evtObject.delta = now - reqStartTimeMs;
             }
-            reqStartTimeMs = std.time.milliTimestamp();
-        } else if (std.mem.startsWith(u8, parsed.value.eventMessage, "Did add")) {
+
+            reqStartTimeMs = now;
+        } else if (std.mem.indexOf(u8, parsed.value.eventMessage, " Did add ") != null) {
             evtObject.type = "grant";
 
             if (reqStartTimeMs > 0) {
                 evtObject.hasDelta = true;
-                evtObject.delta = std.time.milliTimestamp() - reqStartTimeMs;
+                evtObject.delta = now - reqStartTimeMs;
             }
             reqStartTimeMs = 0;
-        } else if (std.mem.startsWith(u8, parsed.value.eventMessage, "Did remove")) {
+        } else if (std.mem.indexOf(u8, parsed.value.eventMessage, " Did remove ") != null) {
+            if (reqStartTimeMs > 0) {
+                continue;
+            }
             evtObject.type = "revoke";
         } else {
             try stderr.print("Unexpected message at {s}: {s}...\n", .{ parsed.value.timestamp, parsed.value.eventMessage });
